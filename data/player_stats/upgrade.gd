6111019643dc9a45ebcdaa6ef577a3722bf1c0e9
MenshikovMakxim <menshikov5254@gmail.com>
2026-01-22 22:01:20 +0200

    готова сторінка покращень

--- data/player_stats/upgrade.gd
+++ data/player_stats/upgrade.gd
@@ -7,12 +7,20 @@ enum StatType { HEALTH, SPEED, DAMAGE }
 @export var stat_to_modify: StatType
 @export var value_mult: float = 0.0 # Наприклад, +10% це 0.1
 
-func apply(stats: StatsDef):
+var changed_stat : Array
+
+func calc_modify(name : String, value: float) -> int:
+	var new_value = value * (1.0 + value_mult)
+	changed_stat = [name, value, new_value]
+	return new_value
+	
+func apply(stats: StatsDef) -> Array:
 	match stat_to_modify:
 		StatType.HEALTH:
-			stats.max_health = int(stats.speed * (1.0 + value_mult))
+			stats.max_health = calc_modify("Health", stats.max_health)
 		StatType.SPEED:
-			stats.damage = int(stats.speed * (1.0 + value_mult))
+			stats.speed = calc_modify("Speed", stats.speed)
 		StatType.DAMAGE:
-			stats.damage = int(stats.damage * (1.0 + value_mult))
+			stats.damage = calc_modify("Damage", stats.damage)
 	print("Upgrade applied: ", upgrade_name)
+	return changed_stat
